5f5f968437db38a1bc900636f272e73776324ba0
imansafari1991 <imansafari33@gmail.com>
2021-11-30 20:20:14 +0330

    Add and Update CQRS

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommand:IRequest
+    {
+
+        
+    }
+}
