13492be49d7986ec528b6e22b8e896b9c20b365e
imansafari1991 <imansafari33@gmail.com>
2021-12-07 23:58:42 +0330

    add rabbitmq and mass transit

---
+++ src/Services/Ordering/Ordering.API/EventCusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventCusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        private readonly ILogger<BasketCheckoutConsumer> _logger;
+
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediator, ILogger<BasketCheckoutConsumer> logger)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+        var result=await _mediator.Send(command);
+            _logger.LogInformation("BasketCheckoutEvent consumed successfullly . Created Order Id : {newOrder}", result);
+        }
+    }
+}
